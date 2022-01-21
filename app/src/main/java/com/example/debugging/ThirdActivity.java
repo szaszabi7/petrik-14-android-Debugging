a690b8e91d89b668cbce1141e75ce737a0fa7b7a
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-21 02:58:13 +0100

    bugfix

--- app/src/main/java/com/example/debugging/ThirdActivity.java
+++ app/src/main/java/com/example/debugging/ThirdActivity.java
@@ -19,6 +19,8 @@ public class ThirdActivity extends AppCompatActivity {
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent vissza = new Intent(ThirdActivity.this, Menu.class);
+                startActivity(vissza);
                 finish();
             }
         });
@@ -26,6 +28,6 @@ public class ThirdActivity extends AppCompatActivity {
 
     public void init()
     {
-        buttonBack = findViewById(R.id.buttonExit);
+        buttonBack = findViewById(R.id.buttonBack);
     }
 }
